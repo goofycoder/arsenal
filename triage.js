3543ec190d5b35a3209c311fe3ace2b6189cba99
goofycoder <goofycoder@users.noreply.github.com>
2015-04-21 16:43:59 -0400

    final fix for indent

--- triage.js
+++ triage.js
@@ -1,17 +1,18 @@
 function foo()
 {
-	var url = "http://example.com";
+    var url = "http://example.com";
 
-	var client = new XMLHttpRequest();
+    var client = new XMLHttpRequest();
 
-	client.open("GET", url, false);
-	client.send();
+    client.open("GET", url, false);
+    
+    client.send();
 }
 
 function parse_json()
 {
-	var json = '{"result":true,"count":1}',
+    var json = '{"result":true,"count":1}',
     obj = JSON.parse(json);
     
-	alert(obj.count);
+    alert(obj.count);
 }
