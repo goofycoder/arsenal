a618fd739ca87231ade43d461d8f7e4e3cddaabc
goofycoder <yanzhou1010@gmail.com>
2015-05-21 15:28:31 -0400

    all CRUD calls in Ruby

--- Ruby/httparty.rb
+++ Ruby/httparty.rb
@@ -1,7 +1,10 @@
+# Before run this, run $json-server db.json first
+
 require 'httparty'
 
 base_uri = 'http://localhost:3000/people'
 
+# CRUD
 # GET
 response = HTTParty.get(base_uri)
 puts response.body, response.code, response.message
@@ -9,7 +12,21 @@ puts response.body, response.code, response.message
 # POST 
 response = HTTParty.post(base_uri,
    	{
-   		:body => { "name"=> "Jack"}.to_json,
+   		:body => { "name"=>"Jack" }.to_json,
    		:headers => { 'Content-Type'=>'application/json', 'Accept'=>'application/json' }
    	})
+puts response.body, response.code, response.message
+
+# PUT
+id_to_update = '10'
+response = HTTParty.put(base_uri + '/' + id_to_update,
+	{
+		:body => { "name" => "Andy" }.to_json,
+		:headers => { 'Content-Type'=>'application/json', 'Accept'=>'application/json' }
+	})
+puts response.body, response.code, response.message
+
+# DELETE
+id_to_delete = '11'
+response = HTTParty.delete(base_uri + '/' + id_to_delete)
 puts response.body, response.code, response.message
