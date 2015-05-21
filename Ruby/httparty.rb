7462f4a0d29bbdf8cc68596a0998d3b4061e6417
goofycoder <yanzhou1010@gmail.com>
2015-05-21 15:21:44 -0400

    post route

--- Ruby/httparty.rb
+++ Ruby/httparty.rb
@@ -1,7 +1,15 @@
 require 'httparty'
 
-url = 'http://localhost:3000/people'
+base_uri = 'http://localhost:3000/people'
 
-response = HTTParty.get(url)
+# GET
+response = HTTParty.get(base_uri)
+puts response.body, response.code, response.message
 
+# POST 
+response = HTTParty.post(base_uri,
+   	{
+   		:body => { "name"=> "Jack"}.to_json,
+   		:headers => { 'Content-Type'=>'application/json', 'Accept'=>'application/json' }
+   	})
 puts response.body, response.code, response.message
