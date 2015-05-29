d573eb2f2524c7074d5f985d9397cb62480c0d8f
goofycoder <yanzhou1010@gmail.com>
2015-05-29 17:14:22 -0400

    select and reject

--- Ruby/map.rb
+++ Ruby/map.rb
@@ -1,4 +1,4 @@
-## difference btwn map and map!
+##### difference btwn map and map!
 #  map: does not change the orginal data
 #  map!:  changes are made in-place
 
@@ -11,3 +11,18 @@ puts numbers    # [1,2,3]
 
 numbers.map! { |n| n*n }
 puts numbers    # [1,4,9] <= numbers are updated
+
+
+##### select
+select_arr  = numbers.select { |n| n==9 }
+puts select_arr         # 9
+
+non_select_arr = numbers.select { |n| n==8 }
+puts non_select_arr     # returns nil, output nothing
+
+##### reject
+new_arr = numbers.reject { |n| n==9 }
+puts new_arr    # [1,4]
+
+numbers.reject! { |n| n==9 }
+puts numbers    # changes made in-place, numbers = [1,4]
