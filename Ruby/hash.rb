4e0bacf71dbfe96f1b60f0e44da19e3678417314
goofycoder <yanzhou1010@gmail.com>
2015-05-30 18:06:29 -0400

    hash basic

---
+++ Ruby/hash.rb
@@ -0,0 +1,12 @@
+ppl = {
+    :firstname => "Tom",
+    :lastname => "Brady",
+    :age => 37,
+    :position => "QB"
+}
+
+puts ppl                # {:firstname=>"Tom", :lastname=>"Brady", :age=>37, :position=>"QB"}
+
+puts ppl[:age]          # => 37
+
+puts ppl[:position]     # => QB
