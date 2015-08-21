4ab2f3b15b041404f783e00e4fadc0e9d556457c
goofycoder <goofycoder@users.noreply.github.com>
2015-08-21 16:58:08 -0400

    parsing of json response

--- nodeJS/rest_call.js
+++ nodeJS/rest_call.js
@@ -15,16 +15,29 @@ console.info('Do the GET call');
 // GET
 var req = https.request(options, function(response) {
     console.log("statusCode: ", response.statusCode);
-	  console.log("headers: ", response.headers);
+    console.log("headers: ", response.headers);
 	
     response.on('data', function(d) {
         console.info('GET result:\n');
         //process.stdout.write(d);
         console.info(JSON.parse(d));
+        
+       	parse(d, foo);
 
         console.info('\n\nCall completed');
     });
 });
+
+var parse = function(data, callback)
+{
+	var d = JSON.parse(data);
+	callback(d['country']);			// 'country' is a key string
+}
+
+var foo = function(data, callback)
+{
+    console.info(data);
+}
 
 req.end();
 req.on('error', function(e) {
